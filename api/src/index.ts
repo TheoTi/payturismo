64f2aef001c44506a8a3fa5d227b29f212b7c5c6
Matheus <matheusti.contato@gmail.com>
2025-03-25 10:19:47 -0300

    define admin routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import "express-async-errors";
+import { adminRoutes } from "./http/routes/adminRoutes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(adminRoutes);
 
 export { app };
