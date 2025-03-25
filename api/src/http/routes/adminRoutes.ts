ad6c251e5c1b59ad2d755fd43ce819c59980c312
Matheus <matheusti.contato@gmail.com>
2025-03-25 15:48:06 -0300

    add route protections

--- api/src/http/routes/adminRoutes.ts
+++ api/src/http/routes/adminRoutes.ts
@@ -3,19 +3,34 @@ import { adminController } from "../controllers/AdminController";
 import { validateRequest } from "../middlewares/validateRequest";
 import { CreateAgencyRequestSchema } from "../../validators/createAgencyZodSchema";
 import { updateAgencyRequestSchema } from "../../validators/updateAgencyZodSchema";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const adminRoutes = Router();
 
-adminRoutes.get("/agency/:", adminController.listAgencies);
-adminRoutes.get("/agency/:id/:", adminController.findAgencyById);
-adminRoutes.delete("/agency/:id/:", adminController.deleteAgency);
+adminRoutes.get("/agency/:", authMiddleware(), adminController.listAgencies);
+
+adminRoutes.get(
+  "/agency/:id/:",
+  authMiddleware(),
+  adminController.findAgencyById
+);
+
+adminRoutes.delete(
+  "/agency/:id/:",
+  authMiddleware(["admin"]),
+  adminController.deleteAgency
+);
+
 adminRoutes.post(
   "/agency/:",
+  authMiddleware(),
   validateRequest(CreateAgencyRequestSchema),
   adminController.createAgency
 );
+
 adminRoutes.put(
   "/agency/:id/:",
+  authMiddleware(),
   validateRequest(updateAgencyRequestSchema),
   adminController.updateAgency
 );
