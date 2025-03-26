6f6715dd16078c28f39fa724fde55cba7140d43c
Matheus <matheusti.contato@gmail.com>
2025-03-26 19:14:44 -0300

    change app fallback

--- web/src/app/Router/index.tsx
+++ web/src/app/Router/index.tsx
@@ -2,12 +2,13 @@ import { Suspense } from "react";
 import { lazyLoad } from "../utils/lazyLoad";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
+import { Fallback } from "@/views/layout/Fallback";
 
 const { SignIn } = lazyLoad(() => import("@/views/pages/SignIn"));
 
 export function Router() {
   return (
-    <Suspense fallback={<div>carregando...</div>}>
+    <Suspense fallback={<Fallback />}>
       <Routes>
         <Route path={routes.signIn} element={<SignIn />} />
       </Routes>
