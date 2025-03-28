44cd02964afba8b06cb9f0039ae1635079523297
Matheus <matheusti.contato@gmail.com>
2025-03-28 19:30:04 -0300

    add swagger ui

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,23 @@
 import "dotenv/config";
 import express from "express";
 import "express-async-errors";
+import fs from "node:fs";
+import yaml from "js-yaml";
+import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
 import { adminRoutes } from "./http/routes/adminRoutes";
 import { publicRoutes } from "./http/routes/publicRoutes";
+// import swaggerDocument from "./docs/openapi.yaml";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const swaggerDocument = yaml.load(fs.readFileSync("./openapi.yaml", "utf8"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument as any));
 
 app.use(adminRoutes);
 app.use(publicRoutes);
